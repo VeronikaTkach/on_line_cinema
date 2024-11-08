49060fb8c1102546c055f0074185af4038b9c002
Milena-Knoll <milena-knoll@mail.ru>
2024-11-09 00:25:35 +0300

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,36 @@
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header/Header';
-import Afisha from './pages/Afisha';
-import Films from './pages/Films';
-import Categories from './pages/Categories';
-import Media from './pages/Media';
-import News from './pages/News';
-import Selections from './pages/Selections';
-import Actors from './pages/Actors';
-import Logo from './pages/Logo';
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Afisha from "./pages/Afisha";
+import Films from "./pages/Films";
+import Categories from "./pages/Categories";
+import Media from "./pages/Media";
+import News from "./pages/News";
+import Selections from "./pages/Selections";
+import Actors from "./pages/Actors";
+import Logo from "./pages/Logo";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Logo />} />
+          <Route path="/afisha" element={<Afisha />} />
+          <Route path="/media" element={<Media />} />
+          <Route path="/films" element={<Films />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/selections" element={<Selections />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
       <Footer />
     </>
   );
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Logo />} />
-        <Route path="/afisha" element={<Afisha />} />
-        <Route path="/media" element={<Media />} />
-        <Route path="/films" element={<Films />} />
-        <Route path="/actors" element={<Actors />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/selections" element={<Selections />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </Router>
-  )
 }
 
 export default App;
